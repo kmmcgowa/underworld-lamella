52bda48e53c504cc7f2e21b9d3d3daa99df5c578
kmmcgowa <kevin@kevin-mcgowan.com>
2018-09-06 16:21:50 -0700

    Home Screen markup and styles

---
+++ src/components/home/HomeFooter.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="home-footer-container flex color-orange">
+    <div class="cta flex action">
+      <img class="icon" src="../../assets/touch_explore.png">
+      <p class="description">Touch a tablet above to explore it, see the translation, and learn more.</p>
+    </div>
+    <div class="audio flex action">
+      <img class="icon" src="../../assets/play_btn.svg">
+      <p class="description">Hear an Orphic tablet inscription, recited in ancient Greek.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+// import Plyr from 'plyr'
+
+export default {
+
+  name: 'HomeFooter',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.home-footer-container {
+  width: 100%;
+  & > div {
+    width: 100%;
+    flex-grow: 1;
+  }
+}
+
+.cta {
+  justify-content: flex-end;
+
+  &:after {
+    content: ' ';
+    width: 2px;
+    background-color: $color-grey;
+    height: 120px;
+    transform: translate(1em, -16px);
+  }
+}
+
+.cta, .audio {
+  padding: 0 1em;
+}
+
+img {
+  width: 90px;
+  height: 90px;
+}
+
+.description {
+  display: inline-block;
+  font-size: 1em;
+  padding-left: .75em;
+  max-width: 15em;
+  height: auto;
+  max-height: 90px;
+  vertical-align: middle;
+  line-height: 45px;
+}
+
+</style>
