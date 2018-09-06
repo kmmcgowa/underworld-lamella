665c9d4521b7c4e87a3bdeaa5bc85e8885d662aa
kmmcgowa <kevin@kevin-mcgowan.com>
2018-09-05 19:32:58 -0700

    Added styles

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="serif">Hello World from Home screen!</h1>
+</template>
+
+<script>
+export default {
+
+  name: 'Home',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
