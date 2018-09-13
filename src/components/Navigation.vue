fc6788037a07eb5c29ecfbccb76a257e6b2eac5f
kmmcgowa <kevin@kevin-mcgowan.com>
2018-09-13 14:36:22 -0700

    Created Top Navigation component

---
+++ src/components/Navigation.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav id="main-navigation" class="flex">
+    <div class="back" @click="goBack">
+      Back
+    </div>
+    <div class="restart" @click="restart">
+      Restart
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navigation",
+
+  methods: {
+    goBack () {
+      this.$router.go(-1)
+    },
+
+    restart () {
+      this.$router.push({name: 'home'})
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+nav {
+  flex-basis: 100%;
+  justify-content: space-between;
+  > div {
+    cursor: pointer;
+  }
+}
+</style>
