e6f319a9f06597d020d7d20f0f0155d595325139
kmmcgowa <kevin@kevin-mcgowan.com>
2018-10-04 16:43:46 -0700

    Added analytics

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import VueAnalytics from 'vue-analytics'
+import { VueHammer } from 'vue2-hammer'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'web-animations-js'
 
 Vue.use(VueAxios, axios)
+Vue.use(VueAnalytics, {
+  // TODO: get ID from env
+  id: 'UA-110656288-3',
+  router
+})
+Vue.use(VueHammer)
 
 Vue.config.productionTip = false
 
